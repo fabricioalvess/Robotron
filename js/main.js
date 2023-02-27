99a312ad658f55117f17020932e127518d026a7a
Fabrício Cintra <fabricioalvescintra@gmail.com>
2023-02-26 23:22:32 -0300

    data-atribiut

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 
-var controle = document.querySelectorAll(".controle-ajuste");
+var controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach((controleComIndiceClicado)=>{
     controleComIndiceClicado.addEventListener("click",(eventoIndiceClicado)=>{
-        manipulaDados(eventoIndiceClicado.target.textContent, eventoIndiceClicado.target.parentNode)
+        manipulaDados(eventoIndiceClicado.target.dataset.controle, eventoIndiceClicado.target.parentNode)
+        console.log(controleComIndiceClicado.target)
     })
 })
 
 
 
  function manipulaDados(controleComIndiceClicado, eventoIndiceClicadoPai){
-    var peca = eventoIndiceClicadoPai.querySelector(".controle-contador")
+    var peca = eventoIndiceClicadoPai.querySelector("[data-contador]")
     if(controleComIndiceClicado === "-"){
         peca.value = parseInt(peca.value) - 1;
     }else{
